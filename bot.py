f1c5c54e814cde38c29aff8142bad7ae4ad78e54
antonsmizh <smizanton04@gmail.com>
2025-04-13 12:57:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from aiohttp import web
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
